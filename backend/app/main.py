d215b29eb973646ed91987b33e71a8637330f511
interstellaroo <michal.figiel03@gmail.com>
2025-08-01 00:34:56 +0200

    Initial commit. Projects setup

---
+++ backend/app/main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Credibility Evaluation",
+    description="API for thesis project - Article Credibility Evaluation",
+    version="v1.0",
+)
+
